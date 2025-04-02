bacbfc097d99a434053c600311cd3ab61b4022c2
dgalytskyi <galitskiy.dima8@gmail.com>
2025-04-02 15:08:26 +0200

    release

---
+++ Homework9/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Homework9;
+
+internal static class Program
+{
+    private static int SearchWordInFile(string filePath, string searchWord)
+    {
+        using var file = new StreamReader(
+            new FileStream(filePath, FileMode.Open, FileAccess.Read));
+
+        string[] tokens = file.ReadToEnd().Split([" ", ", ", ". ", "? ", "! "], StringSplitOptions.RemoveEmptyEntries);
+        
+        return tokens.Count(token => token.Equals(searchWord));
+    }
+    
+    private static void Main(string[] args)
+    {
+        if (args.Length < 2) return;
+        
+        int wordOccurrences = SearchWordInFile(args[0], args[1]);
+        
+        Console.WriteLine(wordOccurrences);
+    }
+}
